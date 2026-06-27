eb3133ab80d8c278fbaea6c79ec3a0c9e68580e2
Mohd Shazwan Norisham <zebwan00@gmail.com>
2026-06-28 02:07:49 +0800

    Fix Vite alias for GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/ivory-dental/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
